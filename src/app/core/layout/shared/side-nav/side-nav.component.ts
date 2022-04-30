2f51b4c585a8d59872ce9a0ad3b12d6bd7ffe249
aFurtadoDev <alexander.furtado.oliveira@gmail.com>
2022-04-29 23:52:13 -0300

    Added layout and adjusted css

--- src/app/core/layout/shared/side-nav/side-nav.component.ts
+++ src/app/core/layout/shared/side-nav/side-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-side-nav',
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-nav.component.scss'],
 })
 export class SideNavComponent implements OnInit {
+  @Output() toggleSidebarForMe: EventEmitter<any> = new EventEmitter();
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit(): void {}
+
+  toggleSidebar(): void {
+    this.toggleSidebarForMe.emit();
+  }
 }
