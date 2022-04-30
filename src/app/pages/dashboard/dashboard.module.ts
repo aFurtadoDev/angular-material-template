4f714170269cf2d911af0106410ac00f08fdb751
aFurtadoDev <alexander.furtado.oliveira@gmail.com>
2022-04-30 00:39:05 -0300

    Preparing Dashboard

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
-
+import { AngularMaterialModule } from '../../core/shared/modules/angular-material/angular-material.module';
 @NgModule({
-  imports: [CommonModule, DashboardRoutingModule],
+  imports: [CommonModule, DashboardRoutingModule, AngularMaterialModule],
   declarations: [],
 })
 export class DashboardModule {}
