8b19d8540033ffdd54a08867155d9ca66a1582a4
aFurtadoDev <64546095+aFurtadoDev@users.noreply.github.com>
2022-10-07 07:03:19 -0300

    1 file modified, 2 added, 2 deleted

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -4,7 +4,7 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { AngularMaterialModule } from '../../core/shared/modules/angular-material/angular-material.module';
 import { DashboardComponent } from './dashboard.component';
 import { DataTableModule } from '../shared/data-table/data-table.module';
-import { TranslateModule } from '@ngx-translate/core';
+
 @NgModule({
   imports: [
     CommonModule,
